a2d95d1a147d5d6a2d989001da8881f456adf845
Tiaan van der Riel <tiaanvdr55@gmail.com>
2023-11-25 15:30:39 +0900

    Heeelppp

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng initialLocation = const LatLng(37.422131, -122.084801);
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
+  @override
+  void initState() {
+    addCustomIcon();
+    super.initState();
+  }
+
+  Future<void> addCustomIcon() async {
+    final http.Response response = await http.get(Uri.parse("https://placehold.co/200x.png"));
+    if (response.statusCode == 200) {
+      setState(() {
+        markerIcon = BitmapDescriptor.fromBytes(response.bodyBytes);
+      });
+    } else {
+      throw Exception('Failed to load marker icon');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: initialLocation,
+          zoom: 14,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("marker1"),
+            position: const LatLng(37.422131, -122.084801),
+            draggable: true,
+            icon: markerIcon,
+          ),
+          Marker(
+            markerId: MarkerId("marker2"),
+            position: LatLng(37.415768808487435, -122.08440050482749),
+            icon: markerIcon,
+          ),
+        },
+      ),
+    );
+  }
+}
