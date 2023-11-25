93143f6984db4c4693cad03f6e28dfd0871271f9
Jonothan Sue <jonothansue@gmail.com>
2023-11-25 19:24:21 +0900

    api:store: add location

--- lib/api/stores.dart
+++ lib/api/stores.dart
@@ -1,10 +1,11 @@
+import 'package:new_mac_test/models/location.dart';
 import 'package:new_mac_test/models/store.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final supabase = Supabase.instance.client;
 
 Future<List<Store>> getStores() async {
-  List<Map<String, dynamic>> res = await supabase.from('store').select();
+  final res = await supabase.from('store').select();
 
   return Store.listFromJson(res);
 }
@@ -28,4 +29,22 @@ Future<Store> updateStore(Store store) async {
     throw response.error!;
   }
   return store;
+}
+
+Future<List<Location>> getLocationsForStore(String storeId) async {
+  final res = await supabase
+      .from('location')
+      .select()
+      .eq('store_id', storeId)
+      .order('created_at', ascending: false);
+  return Location.listFromJson(res);
+}
+
+Future<Location> createLocationForStore(
+    String storeId, Location location) async {
+  final response = await supabase.from('location').insert(location.toJson());
+  if (response.error != null) {
+    throw response.error!;
+  }
+  return location;
 }
