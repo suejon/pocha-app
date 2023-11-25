50f8bd605a288ce3dc2128627532baf2ea07d1f0
Jono Sue <jonothansue@gmail.com>
2023-11-25 20:33:30 +0900

    Task create location and store (#4)
    * api:store: add create, update
    
    * api:store: add location
    
    * api:get:store: retrieve location

--- lib/api/stores.dart
+++ lib/api/stores.dart
@@ -1,10 +1,64 @@
+import 'package:new_mac_test/models/location.dart';
 import 'package:new_mac_test/models/store.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final supabase = Supabase.instance.client;
 
-Future<List<Store>> getStores() async {
-  List<Map<String, dynamic>> res = await supabase.from('store').select();
+Future<List<StoreWithLocation>> getStores() async {
+  final res = await supabase.from('store').select('''
+    id,
+    name,
+    created_at,
+    updated_at,
+    location(latitude, longitude)
+    ''');
 
-  return Store.listFromJson(res);
+  print("result: $res");
+  return StoreWithLocation.listFromJson(res);
+}
+
+Future<(Store, Location)> getStore(String id) async {
+  final store = await supabase.from('store').select().eq('id', id).single();
+  final location = await supabase
+      .from('location')
+      .select()
+      .eq('store_id', id)
+      .order('created_at', ascending: false)
+      .single();
+  return (Store.fromJson(store), Location.fromJson(location));
+}
+
+Future<Store> createStore(Store store) async {
+  final response = await supabase.from('store').insert(store.toJson());
+  if (response.error != null) {
+    throw response.error!;
+  }
+  return store;
+}
+
+Future<Store> updateStore(Store store) async {
+  final response =
+      await supabase.from('store').update(store.toJson()).eq('id', store.id);
+  if (response.error != null) {
+    throw response.error!;
+  }
+  return store;
+}
+
+Future<List<Location>> getLocationsForStore(String storeId) async {
+  final res = await supabase
+      .from('location')
+      .select()
+      .eq('store_id', storeId)
+      .order('created_at', ascending: false);
+  return Location.listFromJson(res);
+}
+
+Future<Location> createLocationForStore(
+    String storeId, Location location) async {
+  final response = await supabase.from('location').insert(location.toJson());
+  if (response.error != null) {
+    throw response.error!;
+  }
+  return location;
 }
--- lib/models/store.dart
+++ lib/models/store.dart
@@ -1,3 +1,5 @@
+import 'package:new_mac_test/models/location.dart';
+
 class Store {
   String? id;
 
@@ -32,5 +34,41 @@ class Store {
 
   static List<Store> listFromJson(List<dynamic> json) {
     return json.map((value) => Store.fromJson(value)).toList();
+  }
+}
+
+class StoreWithLocation extends Store {
+  List<Location> locations = [];
+
+  StoreWithLocation();
+
+  @override
+  String toString() {
+    final locs = locations.toString();
+    return 'Store[id=$id, name=$name, createdAt=$createdAt, updatedAt=$updatedAt, locations=$locs ]';
+  }
+
+  StoreWithLocation.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    locations =
+        json['location'] != null ? Location.listFromJson(json['location']) : [];
+  }
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'created_at': createdAt,
+      'updated_at': updatedAt,
+      'locations': locations
+    };
+  }
+
+  static List<StoreWithLocation> listFromJson(List<dynamic> json) {
+    return json.map((value) => StoreWithLocation.fromJson(value)).toList();
   }
 }
